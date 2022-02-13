08a5c9ade4f6050510ce28c9bee16da387c432d5
kohei0811 <kohei19890811@gmail.com>
2022-02-13 14:43:20 +0900

    新規登録機能、ログイン・ログアウト機能、ユーザーモデル単体テストの実装の修正3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :date_of_birth])
   end
 end
