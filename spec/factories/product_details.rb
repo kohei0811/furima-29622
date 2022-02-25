0526103bb8e5d621c74e0f5b86f33ad8eaf1b391
kohei0811 <kohei19890811@gmail.com>
2022-02-26 00:18:43 +0900

    商品出品機能の実装の修正2

--- spec/factories/product_details.rb
+++ spec/factories/product_details.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     prefecture_id         {2}
     days_to_ship_id       {2}
     price                 {400}
+    association :user
 
     after(:build) do |product_detail|
       product_detail.image.attach(io: File.open('public/images/nagano-The_eruption_of_Mt.Yake-sのコピー.jpg'), filename: 'test_image.jpg')
