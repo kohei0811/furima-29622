6c98c89824e26f9435c8cce30bcf933fdd4a7ac9
kohei0811 <kohei19890811@gmail.com>
2022-02-26 17:58:08 +0900

    商品出品機能の実装の修正4

--- app/models/product_detail.rb
+++ app/models/product_detail.rb
@@ -17,5 +17,5 @@ class ProductDetail < ApplicationRecord
   validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 end
